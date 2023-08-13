59b554cec9514c90e8817e7c49d7e56a64fd98ca
coffinslugs <55113474+coffinslugs@users.noreply.github.com>
2023-08-13 07:06:33 -0600

    Added particle effects to finish line and character

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float reloadSceneDelay = 0.5f;
+    [SerializeField] ParticleSystem finishEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", reloadSceneDelay);
         }
     }
